4af803ad8a9e48595a42b7c6cf8c369f76cb0d97
Daniel Vidal Hussey <daniel.vidal.hussey@gmail.com>
2015-11-12 11:09:37 +0100

    Add initial basicvimrc for large files
    * Add a novimrc alias for it

--- aliases.zsh
+++ aliases.zsh
@@ -48,31 +48,33 @@ zstyle ':completion:*' list-colors 'di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43
 # http://tealscientific.com/blog/?p=2450
 export LSCOLORS=gxfxbEaEBxxEhEhBaDaCaD
 
-alias l='ls -lhoF ${colorflag}'					        # No hidden
-alias ll='ls -lahoF ${colorflag}'				        # +Hidden
-alias lls='ls -lahSoF ${colorflag}'				        # +Hidden +Sort by size
-alias llt='ls -lahtoF ${colorflag}'				        # +Hidden +Sort by modified time
+alias l='ls -lhoF ${colorflag}'		    # No hidden
+alias ll='ls -lahoF ${colorflag}'	    # +Hidden
+alias lls='ls -lahSoF ${colorflag}'	    # +Hidden +Sort by size
+alias llt='ls -lahtoF ${colorflag}'	    # +Hidden +Sort by modified time
 
 alias llist='ls -m'                     # List files in a comma-separated line
 alias lllist='ls -ma'                   # List all files in a comma-separated line
 
 alias mailsize='du -hs ~/Library/mail'
-alias ttop='top -ocpu -R -F -s 2 -n30'		# Top processes
+alias ttop='top -ocpu -R -F -s 2 -n30'  # Top processes
 
-alias tree='tree -AC'	                    # Print pretty lines colourise
+alias tree='tree -AC'	                # Print pretty lines colourise
 alias t='tree -AC'
-alias cp='cp -i'                            # Don't silently overwrite
+alias cp='cp -i'                        # Don't silently overwrite
 
 alias v='vim'
 alias nv='nvim'
+alias novim='vim -u NONE'
 alias p='python'
 alias p3='python3'
-alias ip3="ipython3"
+alias ip3='ipython3'
+alias g='git'
 
 alias h='history'
 
 
-alias please='sudo $(fc -ln -1)'            # http://unix.stackexchange.com/a/158480
+alias please='sudo $(fc -ln -1)'        # http://unix.stackexchange.com/a/158480
 
 # Dotfile configuration
 alias my-zsh="vim ~/.zshrc"
